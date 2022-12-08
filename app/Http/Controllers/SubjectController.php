40637533e760e1ccd855448b65a3eff63b2fe7b6
Mauricio Garnica <edgarmauriciog99@gmail.com>
2022-12-07 19:36:36 -0600

    Se termino el catalogo de materias

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $response_flag = 3;
+        $result = null;
+        $trace = null;
+        try{
+            $subject = new Subject($request->all());
+            $subject->save();
+            $response_flag = 1;
+        }
+        catch (\ErrorException $e) {
+            $response_flag = 2;
+            $result = $e->getMessage();
+            $trace = $e->getTrace();
+        }
+        catch (\Illuminate\Database\QueryException $e) {
+            $response_flag = 2;
+            $result = $e->errorInfo[2];
+            $trace = $e->getTrace();
+        }
+        finally {
+            $data = [
+                "response" => $result,
+                "response_flag" => $response_flag,
+                "trace" => $trace
+            ];
+            return response()->json($data, 200, [JSON_UNESCAPED_UNICODE]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($teacher_id)
+    {
+        $response_flag = 3;
+        $result = null;
+        $trace = null;
+        try{
+            $subjects = Subject::where('teacher_id', '=', $teacher_id);
+            $result = $subjects;
+            $response_flag = 1;
+        }
+        catch (\ErrorException $e) {
+            $response_flag = 2;
+            $result = $e->getMessage();
+            $trace = $e->getTrace();
+        }
+        catch (\Illuminate\Database\QueryException $e) {
+            $response_flag = 2;
+            $result = $e->errorInfo[2];
+            $trace = $e->getTrace();
+        }
+        finally {
+            $data = [
+                "response" => $result,
+                "response_flag" => $response_flag,
+                "trace" => $trace
+            ];
+            return response()->json($data, 200, [JSON_UNESCAPED_UNICODE]);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $response_flag = 3;
+        $result = null;
+        $trace = null;
+        try{
+            $subject = Subject::find($id);
+            $subject->name = $request->name;
+            $subject->save();
+            $response_flag = 1;
+        }
+        catch (\ErrorException $e) {
+            $response_flag = 2;
+            $result = $e->getMessage();
+            $trace = $e->getTrace();
+        }
+        catch (\Illuminate\Database\QueryException $e) {
+            $response_flag = 2;
+            $result = $e->errorInfo[2];
+            $trace = $e->getTrace();
+        }
+        finally {
+            $data = [
+                "response" => $result,
+                "response_flag" => $response_flag,
+                "trace" => $trace
+            ];
+            return response()->json($data, 200, [JSON_UNESCAPED_UNICODE]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $response_flag = 3;
+        $result = null;
+        $trace = null;
+        try{
+            $subject = Subject::find($id);
+            $subject->delete();
+            $response_flag = 1;
+        }
+        catch (\ErrorException $e) {
+            $response_flag = 2;
+            $result = $e->getMessage();
+            $trace = $e->getTrace();
+        }
+        catch (\Illuminate\Database\QueryException $e) {
+            $response_flag = 2;
+            $result = $e->errorInfo[2];
+            $trace = $e->getTrace();
+        }
+        finally {
+            $data = [
+                "response" => $result,
+                "response_flag" => $response_flag,
+                "trace" => $trace
+            ];
+            return response()->json($data, 200, [JSON_UNESCAPED_UNICODE]);
+        }
+    }
+}
